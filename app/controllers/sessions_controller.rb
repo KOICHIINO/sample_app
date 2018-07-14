2a0ba79623ef7a1892b2bfe12f883f8e7a86fad8
KOICHI INO <kouichi02913@gmail.com>
2018-07-15 00:49:47 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,19 @@
 class SessionsController < ApplicationController
 
   def new
+      # debugger
   end
 
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
+    #@user で良いのか確認！！！（課題）
     if @user && @user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_back_or @user
       # remember user
-      redirect_to @user
+      # redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
       render 'new'
