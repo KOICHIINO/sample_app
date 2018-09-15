79eddc223e2d7464b8e12a58c1a0d1955abdba4a
KOICHI INO <kouichi02913@gmail.com>
2018-09-15 19:28:20 +0900

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'bootstrap-sass', '3.3.7'
 gem 'faker',          '1.7.3'
 gem 'will_paginate',           '3.1.6'
 gem 'bootstrap-will_paginate', '1.0.0'
+gem 'carrierwave',             '1.2.2'
+gem 'mini_magick',             '4.7.0'
 
 
 
@@ -39,6 +41,7 @@ end
 
 group :production do
   gem 'pg', '0.20.0'
+  gem 'fog', '1.42'
 end
 
 # Windows環境ではtzinfo-dataというgemを含める必要があります
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include SessionsHelper
+  
+  private
+  
+  # before アクション
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location    #アクセスしようとしたリクエストを覚えておく（GETのみ）
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+  
 end
